a3ad3d6004f34aa174b95a4559402b8c797fb24d
kngandar <kngandar@gmail.com>
2021-01-13 17:31:37 -0500

    Added ability score logic

--- src/components/AbilityScore.vue
+++ src/components/AbilityScore.vue
@@ -3,7 +3,12 @@
 
 <template>
     <div>
-        Ability Score
+
+        <h2>Ability Score</h2>
+        <div v-for="item in abscores" :key="item.text">
+            {{item.text}} <input v-on:change="input_check(item)" v-model="item.val" /> MOD: {{get_mod(item)}}
+        </div>
+        {{msg}}
 
     </div>
 </template>
@@ -12,14 +17,60 @@
 import Vue from "vue";
 import Component from "vue-class-component";
 
+interface ABSCORE {
+    text: string;
+    val: number;
+}
+
 @Component({
     
 })
 
 export default class AbilityScore extends Vue {
 
+    public abscores: ABSCORE[] = [];
+    public stats: string[] = ['STR','DEX','CON','INT','WIS','CHA'];
+    public msg: string = "";
+
+
+    // Runs on load
     constructor() {
         super();
+    }
+
+    // Functions (Methods)
+    public mounted(){
+        this.load();
+    }
+    
+    public load(){
+        console.log("Initialize scores")
+        
+        var idx;
+
+        for (idx = 0; idx < this.stats.length; idx++){
+            let abs: ABSCORE = {
+                text: this.stats[idx],
+                val: 0
+            };
+
+            this.abscores.push(abs);
+        }
+        
+    }
+
+    public input_check(abs:ABSCORE){
+        console.log("Checking for valid input");
+        this.msg = "";
+        
+        if (!(abs.val >= 0 && abs.val <= 20)){
+            abs.val = 0;
+            this.msg = "Input must only be integers";
+        }
+    }
+
+    public get_mod(abs:ABSCORE){
+        return Math.floor((abs.val-10)/2);
     }
 
 }
